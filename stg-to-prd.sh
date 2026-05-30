e5df5eaed47fc91ec81a73e39c6ba455caf5d995
Vlsdimir Lamtev <lamtev.igvl@gmail.com>
2026-05-30 08:42:23 +0300

    final version

--- stg-to-prd.sh
+++ stg-to-prd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "start pushing stg to prd"
-git checkout stg
-git merge dev --no-edit
+git checkout prd
+git merge stg --no-edit
 TAG_NAME="release-$(date +%Y%m%d-%H%M%S)"
 git tag "$TAG_NAME"
-git push origin stg
+git push origin prd
 git push origin "$TAG_NAME"
 git checkout dev
 echo "end pushing"
