282258a1c0807c503d7336f489efe420e7be8f32
Vlsdimir Lamtev <lamtev.igvl@gmail.com>
2026-05-30 08:02:41 +0300

    final version2 20260530-080241

--- dev-to-stg.sh
+++ dev-to-stg.sh
@@ -4,7 +4,8 @@ git checkout stg
 git merge dev --no-edit
 git tag -a v -m "stg-$(date +%Y%m%d-%H%M%S)"
 git tag -l
-git push origin stg 
+git commit -m "version $(date +%Y%m%d-%H%M%S)"
+git push origin stg
 git push origin tags
 git checkout dev
 echo "end dev->stg"
