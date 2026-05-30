086371490406ca47e80b37ed76bf04bf9a1b22fb
Vlsdimir Lamtev <lamtev.igvl@gmail.com>
2026-05-30 07:29:09 +0300

    final version

--- dev-to-stg.sh
+++ dev-to-stg.sh
@@ -3,8 +3,8 @@ echo "start dev->stg"
 git checkout stg
 git merge dev --no-edit
 TAG_NAME = "обновленов в $(date)"
-git tag "$TAG_NAME"
-git push origin stg
+git tag -a "$TAG_NAME"
+git push origin stg -- tags
 git push origin "$TAG_NAME"
 git checkout dev
 echo "end dev->stg"
